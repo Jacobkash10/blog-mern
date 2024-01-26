b94d52ffdaf4a20201bc2cff913bfc228c066763
Jacobkash10 <86664678+Jacobkash10@users.noreply.github.com>
2024-01-26 15:27:11 +0100

    create user model

--- api/index.js
+++ api/index.js
@@ -13,5 +13,5 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express()
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000')
 })
